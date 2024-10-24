1447fabfecba6cde31221707c6fa90075060c623
IvayloKraev <ivaylokraev@protonmail.com>
2024-10-24 15:57:28 +0300

    Add status component

--- inc/wifiCtrl.h
+++ inc/wifiCtrl.h
@@ -5,7 +5,19 @@
 #include "pico/stdio_usb.h"
 #include "pico/cyw43_arch.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "status.h"
+
 #define CYW43_COUNTRY_BULGARIA CYW43_COUNTRY('B', 'G', 0)
+
+typedef struct {
+    const char *ssid;
+    const char *password;
+} wifiConfig_t;
+
+typedef wifiConfig_t *wifiConfigHandler_t;
 
 void curiosity_wifiCtrl_init();
 
